1eea731cad100af0059af4a033cd6592ca607f0f
Diona Mulaj <diona1mulaj@gmail.com>
2026-04-30 10:23:29 +0200

    synlig timer

--- praktisk-prøve-eksamensoppgave-26/Build-A-Snowman/scripts/timeout.gd
+++ praktisk-prøve-eksamensoppgave-26/Build-A-Snowman/scripts/timeout.gd
@@ -1,4 +1,10 @@
 extends Node2D
 
-func _on_timer_timeout() -> void:
+@onready var label = $CanvasLayer/Label
+@onready var timer = $timer
+
+func _process(delta):
+	label.text = "Time: " + str(int(timer.time_left))
+
+func _on_timer_timeout():
 	get_tree().change_scene_to_file("res://ResultScreens/scenes/defeat-screen.tscn")
